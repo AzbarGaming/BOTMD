b4f521068c0850179bbd4aaffc6cd7a07a127423
AzbarGaming <108172283+AzbarGaming@users.noreply.github.com>
2022-06-25 14:36:38 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Abay.
+   * Contact Me on wa.me/62895322697437
+   * Follow https://github.com/AzbarGaming
 */
 
 require('./settings/config')
